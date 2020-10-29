c9573c04b39e1651ea4b487b3c3044c35c021db0
Wayne Cho <waynecho2004@gmail.com>
2020-10-29 11:19:22 -0400

    update node version

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000;
 //Database
 //___________________
 // How to connect to the database either via heroku or locally
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/'+ 'YOUR-DATABASE-NAME';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/'+ 'ga-project2';
 // Connect to Mongo
 mongoose.connect(MONGODB_URI ,  { useNewUrlParser: true});
 // Error / success
