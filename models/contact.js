d4b3ee0a1858c871247fa044cbfde4a7c979484a
Wayne Cho <waynecho2004@gmail.com>
2020-10-30 11:26:57 -0400

    add schema for contact

---
+++ models/contact.js
@@ -0,0 +1,27 @@
+/**
+ * Contact and Child is embbed
+ */
+const mongoose = require('mongoose');
+
+const childSchema = new mongoose.Schema({
+    name: String,
+    age: String
+})
+
+const contactSchema = new mongoose.Schema(
+    {
+        firstName: { type: String, required: true },
+        lastName: String,
+        address: String,
+        state: String,
+        phone: { type: String, required: true },
+        email: String,
+        children: [childSchema],
+    },
+    { timestamps: true }
+);
+
+const Child = mongoose.model('Child', childSchema);
+const Contact = mongoose.model('Contact', contactSchema);
+
+module.exports = { Contact, Child }
