2d344fd985d9ff4f9b91168f1dfa89abb0d38469
Wayne Cho <waynecho2004@gmail.com>
2020-10-30 16:17:51 -0400

    add embedded child to contact

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -63,4 +63,20 @@ router.delete('/:id',  async (req, res) => {
     });
 });
 
+// Create Child Embedded in Contact
+router.post('/:contactId/childs', (req, res) => {
+    //  console.log(req.body);
+    const newChild = new Child(
+        { 
+            name: req.body.name,
+            age: req.body.age
+        });
+    Contact.findById(req.params.contactId, (error, foundContact) => {
+        foundContact.children.push(newChild);
+        foundContact.save((err, updatedContact) => {
+            res.redirect(`/contacts/${updatedContact.id}`);
+        });
+    });
+})
+
 module.exports = router;
