183de39763dee910f1b909a1497082daaa416040
Wayne Cho <waynecho2004@gmail.com>
2020-11-02 14:39:19 -0500

    organization - add list of contacts

--- controllers/organizationController.js
+++ controllers/organizationController.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const Organization = require('../models/organization');
-const Contact = require('../models/contact');
+const Contact = require('../models/contact').Contact;
 
 // Index
 router.get('/', (req, res) => {
@@ -25,22 +25,20 @@ router.post('/', async (req, res) => {
 // Show Organization Detail
 router.get('/:id', async (req, res) => {
     const id = req.params.id;
-    await Organization.findById(id, (error, foundOrganization) => {
-        if (error) res.send(error)
-        res.render('organizations/show.ejs', {
-            organization: foundOrganization,
-        })
+    let foundOrg = await Organization.findById(id).populate('contacts');
+
+    res.render('organizations/show.ejs', {
+        organization: foundOrg,
     });
 });
 
 // Edit page
 router.get('/:id/edit', async (req, res) => {
     const id = req.params.id;
-    await Organization.findById(id, (error, foundOrg) => {
-        if (error) res.send(error)
-        res.render('organizations/edit.ejs', {
-            organization: foundOrg,
-        });
+    let foundOrg = await Organization.findById(id).populate('contacts');
+
+    res.render('organizations/edit.ejs', {
+        organization: foundOrg,
     });
 });
 
@@ -53,5 +51,53 @@ router.put('/:id', async (req, res) => {
         res.redirect('/organizations');
     });
 });
+
+// Show contacts that can add to organization
+router.get('/:id/contacts/new', async (req, res) => {
+    const id = req.params.id;
+    let allContacts = await Contact.find({});
+
+
+    let foundOrg = await Organization.findById(id).populate('contacts');
+   // let foundOrg = await Organization.findById(id).populate('contact', 'firstName').exec();
+   console.log(foundOrg)
+
+    // filter to find available contacts not associated with this organization
+    let filtedContacts = allContacts.filter(contact => {
+        let existingContactIds = foundOrg.contacts.map(e => e._id);
+        if (!existingContactIds.includes(contact.id)) {
+            return contact;
+        }
+    });
+
+    res.render('organizations/add-contacts.ejs', {
+        organization: foundOrg,
+        contacts: filtedContacts,
+    });
+
+});
+
+// Add contacts to organization
+router.put('/:id/contacts', async (req, res) => {
+    const orgId = req.params.id;
+    const newContacts = req.body.contacts;
+    
+    try {
+        let foundOrg = await Organization.findByIdAndUpdate(
+            orgId,
+            {
+            $push: {  // The $push will add new contacts to the list 
+                contacts: newContacts,
+            },
+            },
+            { new: true, upsert: true }
+        );
+        res.redirect(`/organizations/${foundOrg.id}`);
+    } catch (error) {
+        res.send(error);
+    }
+
+});
+
 
 module.exports = router;
--- models/organization.js
+++ models/organization.js
@@ -2,8 +2,6 @@ const mongoose = require('mongoose');
 
 const organizationSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    description: String,
-    address: String,
     contacts: [
         {
             type: mongoose.Schema.Types.ObjectId,
